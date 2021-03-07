7544db778c7cb705faee2fd6cd28058dec08ca76
Benjamin Van Renterghem <benjamin@bvrcode.com>
2021-03-07 18:12:09 +0100

    Update head & header content

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,16 +2,16 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'BVR Code | Freelance Software Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to the website of BVR Code', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Benjamin',
+  subtitle: 'I\'m a Freelance Software Developer',
   cta: '',
 };
 
