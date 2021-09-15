031c0e669ff47a9fe948b9848fb1debda4788d38
BenjaVR <BenjaVR@users.noreply.github.com>
2021-09-15 23:23:05 +0000

    deploy: 6049c6dda93eda89e163d22b86e86e84d39eccc9

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7bf996fa852c4d8a88ab.js"
+    "url": "webpack-runtime-b2fb982d61b46e89827c.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "dcba2cbdcd0659a58c485fd327ecf856"
+    "revision": "f9d909d08904a4c43b4495ac7ccd7006"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
