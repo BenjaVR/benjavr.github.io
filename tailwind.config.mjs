b7965307d9e5c84e41974250b88ee13bbaf3d572
Benjamin Van Renterghem <benjamin@bvrcode.com>
2024-04-23 07:07:50 +0200

    Coming soon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+			colors: {
+				brand: {
+					DEFAULT: '#3786e5',
+					darker: '#317cde',
+					dark: '#285fac'
+				}
+			}
+		},
+  },
+  plugins: [],
 }
